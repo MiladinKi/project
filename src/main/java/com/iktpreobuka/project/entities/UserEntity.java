56011486155085b6ebe3ca784940b3264c08e694
MiladinKi <miladinkovacevic12@gmail.com>
2023-09-25 22:17:31 +0200

    UserBranch
    Uradjeni UserEntity i UserRepository, kao i potrebne metode

---
+++ src/main/java/com/iktpreobuka/project/entities/UserEntity.java
@@ -0,0 +1,99 @@
+package com.iktpreobuka.project.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@Column(nullable = false)
+	private String firstName;
+	@Column(nullable = false)
+	private String lastName;
+	@Column(nullable = false)
+	private String username;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private EUserRole userRole;
+
+	public UserEntity() {
+		super();
+	}
+
+	public UserEntity(Integer id, String firstName, String lastName, String username, String password, String email,
+			EUserRole userRole) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.userRole = userRole;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public EUserRole getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(EUserRole userRole) {
+		this.userRole = userRole;
+	}
+
+}
